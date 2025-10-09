0ad691e4d166779d3c2360579f9cab9e43aee4f8
ssssota <tommysouta@gmail.com>
2025-10-09 20:06:23 +0900

    Initial Commit

---
+++ OpenAlbumTests/OpenAlbumTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAlbumTests.swift
+//  OpenAlbumTests
+//
+//  Created by Tomikawa Sotaro on 2025/10/09.
+//
+
+import Testing
+@testable import OpenAlbum
+
+struct OpenAlbumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
