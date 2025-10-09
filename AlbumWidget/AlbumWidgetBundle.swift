ec36ca6d17b23558176cb9b5ffb560a0218b8d06
ssssota <tommysouta@gmail.com>
2025-10-09 20:14:59 +0900

    Setup project

---
+++ AlbumWidget/AlbumWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumWidgetBundle.swift
+//  AlbumWidget
+//
+//  Created by Tomikawa Sotaro on 2025/10/09.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AlbumWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AlbumWidget()
+        AlbumWidgetControl()
+    }
+}
--- OpenAlbum/OpenAlbumApp.swift
+++ OpenAlbum/OpenAlbumApp.swift
@@ -5,28 +5,28 @@
 //  Created by Tomikawa Sotaro on 2025/10/09.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct OpenAlbumApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      Item.self
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
     }
+  }()
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
 }
